d9780a346cfdc28567dca7a6366493248a879ad8
Pauli Hartala <paulivil@msn.com>
2020-11-20 00:58:27 +0200

    cleaning up

--- package/boot/uboot-zyxel/files/board/ruby/config.mk
+++
@@ -1,6 +0,0 @@
-# Place text to the beginning of SRAM.
-# flip memmap configuration.
-TEXT_BASE=0x88000000
-
-CONFIG_LZMA=y
-
--- package/boot/uboot-zyxel/files/board/ruby_mini/config.mk
+++
@@ -1,5 +0,0 @@
-# Place text to the beginning of SRAM.
-# flip memmap configuration.
-RUBY_MINI_TEXT_BASE=0x88030000
-RUBY_MINI_PIGGY_TEXT_BASE=0x88000000
-
